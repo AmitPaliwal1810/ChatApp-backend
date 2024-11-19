3e1571e1de269c2a1ebd834a23d82ff403e5acf2
Amit Paliwal <amitpaliwal259@gmail.com>
2024-11-20 00:58:08 +0530

    update

--- routes/contactsRoutes.js
+++ routes/contactsRoutes.js
@@ -1,9 +1,13 @@
 import { Router } from "express";
-import { searchContacts } from "../controllers/contactsController.js";
+import {
+  getContactForDMList,
+  searchContacts,
+} from "../controllers/contactsController.js";
 import { verifyToken } from "../middlewares/authMiddleware.js";
 
 const contactsRoutes = Router();
 
 contactsRoutes.post("/search", verifyToken, searchContacts);
+contactsRoutes.get("/get-contact-for-dm", verifyToken, getContactForDMList);
 
 export default contactsRoutes;
